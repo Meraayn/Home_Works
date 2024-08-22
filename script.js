7b9d4b3783f2dbae5452def3800946119535d843
Ainazik <aknazarova.ainazik@gmail.com>
2024-08-23 01:57:22 +0300

    for-while

--- script.js
+++ script.js
@@ -1,2 +1,60 @@
-console. log('Hello World');
-console. log('Hello JS3');
+//! Task 1
+
+// let number = 0
+
+// while (number <= 100){
+//     console.log(number);
+//     number++
+    
+// }
+
+//!Task 2
+
+// for (let number = 0; number <= 100; number+=2) {
+//     console.log(number);
+    
+// }
+
+//! Task 3
+// for (let number = 1; number <= 100; number++) {
+//     if(number % 2 !== 0)
+// console.log(number);
+
+// }
+
+
+//!Task 4
+
+// const str = 'Naruto Shipuden';
+// let letter = 'u';
+
+// console.log(str.includes(letter));
+
+
+//!Task 5
+
+ let userName = "Naruto Shipuden"
+
+ let counter = userName.length;
+
+ while (counter >= 0) {
+    console.log(userName[counter]);
+
+    counter--;
+ }
+
+
+
+
+
+//!task 6
+
+//  let userName = "Naruto Shipuden"
+
+//  let counter = userName.length;
+
+//  while (counter >= 0) {
+//     console.log(userName[counter]);
+
+//     counter--;
+//  }
